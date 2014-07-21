64d3f98a0b058ad8856226ce20f991b36b685d1f
L42y <423300@gmail.com>
2014-07-22 00:48:07 +0800

    feat: setFitView method

--- angular-amap-map.js
+++ angular-amap-map.js
@@ -4,13 +4,19 @@ angular.module('l42y.amap.map', [
 ) {
   return {
     scope: {
+      fitView: '=amapMapFitView'
     },
     restrict: 'EA',
     template: '<div ng-transclude></div>',
     transclude: true,
     controller: function ($scope, $element, $attrs) {
+      var self = this;
       var mapOptions = $scope.$eval($attrs.amapMapOptions);
-      this.map = new $window.AMap.Map($element[0], mapOptions);
+      self.map = new $window.AMap.Map($element[0], mapOptions);
+
+      $scope.$watchCollection('fitView', function (overlays) {
+        if (overlays) self.map.setFitView(overlays);
+      });
     },
     controllerAs: 'amap'
   };
