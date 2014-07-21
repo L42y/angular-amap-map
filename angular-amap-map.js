5cff978f1e6649f30e379e029ea9c5e7d26f92ab
L42y <423300@gmail.com>
2014-07-22 00:39:10 +0800

    refactor: use isolate scope

--- angular-amap-map.js
+++ angular-amap-map.js
@@ -3,7 +3,11 @@ angular.module('l42y.amap.map', [
   $window
 ) {
   return {
+    scope: {
+    },
     restrict: 'EA',
+    template: '<div ng-transclude></div>',
+    transclude: true,
     controller: function ($scope, $element, $attrs) {
       var mapOptions = $scope.$eval($attrs.amapMapOptions);
       this.map = new $window.AMap.Map($element[0], mapOptions);
